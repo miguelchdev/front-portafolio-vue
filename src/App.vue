226b8c63898480adcc3f97f9b069479b636d75a2
miguelangelchgz <miguelangelchgz@gmail.com>
2019-01-31 04:00:03 -0400

    Added bootstrap-vue, created custom boostrap sass file

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <p>ksdjfskdf</p>
-    <button class="btn">gkhdkfg</button>
+    <b-navbar toggleable="md" type="light" variant="info">
+      <b-navbar-toggle target="nav_collapse" />
+
+      <b-navbar-brand href="#"> NavBar </b-navbar-brand>
+
+      <b-collapse id="nav_collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#"> Link </b-nav-item>
+          <b-nav-item href="#" disabled> Disabled </b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input
+              size="sm"
+              class="mr-sm-2"
+              type="text"
+              placeholder="Search"
+            />
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">
+              Search
+            </b-button>
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#"> EN </b-dropdown-item>
+            <b-dropdown-item href="#"> ES </b-dropdown-item>
+            <b-dropdown-item href="#"> RU </b-dropdown-item>
+            <b-dropdown-item href="#"> FA </b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using button-content slot -->
+            <template slot="button-content">
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#"> Profile </b-dropdown-item>
+            <b-dropdown-item href="#"> Signout </b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <b-button variant="primary" size="sm"> I am a Button </b-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style lang="scss">
+@import "./assets/custom-bootstrap.scss";
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -38,6 +38,7 @@ export default {
     msg: String
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
