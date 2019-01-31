38df76f053976da2d67875168ca4042fa060f3a2
miguelangelchgz <miguelangelchgz@gmail.com>
2019-01-31 06:13:15 -0400

    Created header page structure

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,38 @@
 <template>
-  <div id="app">
-    <b-navbar toggleable="md" type="light" variant="info">
-      <b-navbar-toggle target="nav_collapse" />
-
-      <b-navbar-brand href="#"> NavBar </b-navbar-brand>
-
-      <b-collapse id="nav_collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item href="#"> Link </b-nav-item>
-          <b-nav-item href="#" disabled> Disabled </b-nav-item>
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input
-              size="sm"
-              class="mr-sm-2"
-              type="text"
-              placeholder="Search"
-            />
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">
-              Search
-            </b-button>
-          </b-nav-form>
-
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#"> EN </b-dropdown-item>
-            <b-dropdown-item href="#"> ES </b-dropdown-item>
-            <b-dropdown-item href="#"> RU </b-dropdown-item>
-            <b-dropdown-item href="#"> FA </b-dropdown-item>
-          </b-nav-item-dropdown>
-
-          <b-nav-item-dropdown right>
-            <!-- Using button-content slot -->
-            <template slot="button-content">
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#"> Profile </b-dropdown-item>
-            <b-dropdown-item href="#"> Signout </b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <b-button variant="primary" size="sm"> I am a Button </b-button>
-  </div>
+<div id="app">
+  <header class="dark-zone">
+    <b-container fluid>
+      <b-row class="align-items-center">
+        <b-col lg="7" md="12">
+          <b-row>
+            <b-col col md="5">
+              <b-button size="lg" variant="primary" block>
+                Home
+              </b-button>
+            </b-col>
+            <b-col md="5">
+              <b-button size="lg" variant="outline-light" block>
+                About
+              </b-button>
+            </b-col>
+          </b-row>
+        </b-col>
+        <b-col lg="5" md="12">
+          <p>prueba</p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </header>
+</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HeaderImage from "./components/HeaderImage.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    HeaderImage
   }
 };
 </script>
@@ -65,10 +41,28 @@ export default {
 @import "./assets/custom-bootstrap.scss";
 @import "../node_modules/bootstrap/scss/bootstrap.scss";
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    height: 100%;
+}
+header {
+    height: 100%;
+}
+.dark-zone {
+    background-color: $black;
+    color: $white;
+}
+// Extra small devices (portrait phones, less than 576px)
+@media (max-width: 575.98px) {
+    header {
+        height: 40%;
+    }
+}
+// Medium devices (tablets, 768px and up) IPAD Kindle etc
+@media (min-width: 768px) and (max-width: 991.98px) {
+    header {
+        height: 40%;
+    }
 }
 </style>
