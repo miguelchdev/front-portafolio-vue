21d996ea22c8619faa45b21584d86aeeafe2ed93
miguelchdev <miguelangelchgz@gmail.com>
2020-05-01 03:35:20 -0400

    Moved router and fetch before enter in route

--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify';
-import axios from 'axios';
-import Transitions from 'vue2-transitions'
-Vue.use(Transitions)
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import axios from "axios";
+import Transitions from "vue2-transitions";
+Vue.use(Transitions);
+axios.defaults.baseURL = "http://127.0.0.1:8000";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    vuetify,
+    render: (h) => h(App),
+}).$mount("#app");
