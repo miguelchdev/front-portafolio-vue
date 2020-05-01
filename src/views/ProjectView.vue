604e100f46a37f05de17b58b1d5b46f23f8af0a6
miguelchdev <miguelangelchgz@gmail.com>
2020-05-01 04:31:09 -0400

    Projec DEtail view

---
+++ src/views/ProjectView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-dark full-view">
+        <h1>{{project.title}}</h1>
+        <p>{{project.description}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ProjectView",
+    props: {
+        project: { type: Object, required: true }
+    }
+};
+</script>
