ad031f5636327a7180bd183273aa94eeb6bfdbcc
miguelchdev <miguelangelchgz@gmail.com>
2020-05-14 02:46:15 -0400

    Delete project view

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,7 +1,26 @@
 <template>
-    <div class="bg-dark full-view">
-        <h1>{{project.title}}</h1>
-        <p>{{project.description}}</p>
+    <div class="bg-dark d-flex flex-grow-1">
+        <v-container class="px-sm-12 px-5 padding-y parent-height ">
+            <h1 class="text-sm-center text-center">{{project.title}}</h1>
+            <v-carousel
+                cycle
+                height="400"
+                class="my-5"
+                hide-delimiter-background
+                show-arrows-on-hover
+            >
+                <v-carousel-item
+                    v-for="image in project.images"
+                    :key="image.id"
+                    :src="image.file['1366']"
+                    transition="fade-transition"
+                >
+
+                </v-carousel-item>
+            </v-carousel>
+            <p>{{project.description}}</p>
+        </v-container>
+
     </div>
 </template>
 
@@ -13,3 +32,8 @@ export default {
     }
 };
 </script>
+<style lang="scss" scoped>
+.full-view {
+    min-height: 100vh;
+}
+</style>
