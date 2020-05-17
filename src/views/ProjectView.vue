241f4763cfa71f8b0868bafcab4e003ce74c496d
miguelchdev <miguelangelchgz@gmail.com>
2020-05-17 04:55:43 -0400

    Project view page

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -18,7 +18,44 @@
 
                 </v-carousel-item>
             </v-carousel>
+            <v-row class="my-5">
+                <v-col>
+                    <p class="text-uppercase font-weight-bold">Date</p>
+                    <p>-{{project.date}}</p>
+                </v-col>
+                <v-col>
+                    <p class="text-uppercase font-weight-bold">Technologys</p>
+                    <div class="d-flex mx-auto">
+                        <p
+                            :key="key"
+                            class="pr-2"
+                            v-for="(item,key) in project.technologys"
+                        >{{item}} </p>
+                    </div>
+
+                </v-col>
+            </v-row>
             <p>{{project.description}}</p>
+            <h1 class="text-sm-center text-center">{{project.title}}</h1>
+            <v-row class="mx-auto">
+                <v-col
+                    v-for="image in project.images"
+                    :key="image.id"
+                    cols="4"
+                >
+                    <v-card
+                        flat
+                        tile
+                        class="d-flex"
+                    >
+                        <v-img
+                            :src="image.file['1366']"
+                            :lazy-src="image.file['1366']"
+                            :aspect-ratio="4/3"
+                        ></v-img>
+                    </v-card>
+                </v-col>
+            </v-row>
         </v-container>
 
     </div>
