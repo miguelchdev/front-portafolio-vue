e9a674fe557fb149ff23e1b0a097768432f67024
miguelchdev <miguelangelchgz@gmail.com>
2020-06-28 05:00:39 -0400

    Added i18n

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,18 @@
 module.exports = {
     transpileDependencies: ["vuetify"],
+
     configureWebpack: {
         devtool: "source-map",
     },
 
     lintOnSave: true,
+
+    pluginOptions: {
+      i18n: {
+        locale: 'en',
+        fallbackLocale: 'en',
+        localeDir: 'locales',
+        enableInSFC: true
+      }
+    }
 };
