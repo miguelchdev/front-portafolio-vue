84dae56ad23b6dc5fe6a7487a912fefa378884e0
miguelchdev <miguelangelchgz@gmail.com>
2020-07-16 10:41:25 -0400

    Animation load

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,32 @@
 <template>
     <div>
         <introduction v-bind="introduction"></introduction>
-        <service v-bind="services"></service>
-        <project v-bind="projects"></project>
-        <contact v-bind="contact"></contact>
+        <v-lazy
+            v-model="isVisibleService"
+            :options="options"
+            min-height="100vh"
+            :transition="transition"
+        >
+            <service v-bind="services"></service>
+        </v-lazy>
+        <v-lazy
+            v-model="isVisibleProject"
+            :options="options"
+            min-height="100vh"
+            :transition="transition"
+        >
+            <project v-bind="projects"></project>
+        </v-lazy>
+
+        <v-lazy
+            v-model="isVisibleContact"
+            :options="options"
+            min-height="100vh"
+            :transition="transition"
+        >
+            <contact  v-bind="contact"></contact>
+        </v-lazy>
+
     </div>
 </template>
 
@@ -17,6 +40,18 @@ import { mapActions, mapState } from "vuex";
 export default {
     name: "Home",
     components: { Introduction, Service, Project, Contact },
+    data() {
+        return {
+            isActive: false,
+            isVisibleService: false,
+            isVisibleProject: false,
+            isVisibleContact: false,
+            transition:"slide-x-transition",
+            options:{
+                threshold: .20
+            }
+        };
+    },
     computed: {
         ...mapState("pages", [
             "services",
@@ -26,7 +61,7 @@ export default {
         ])
     },
     methods: {
-        ...mapActions("pages", ["fetchPages"])
+        ...mapActions("pages", ["fetchPages"]),
     },
     created() {
         this.fetchPages();
