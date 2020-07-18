014a859b7b9cf961aa4c89bd890d7038298f3abc
miguelchdev <miguelangelchgz@gmail.com>
2020-07-18 03:46:02 -0400

    done services animations

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -1,30 +1,68 @@
 <template>
     <div class="bg-dark full-view">
         <v-container class="px-sm-12 px-5 padding-y parent-height d-flex flex-column">
-            <h1
-                v-if="title"
-                class="text-sm-justify text-center"
-            >{{ title }}</h1>
-            <p
-                v-if="page_contents"
-                class="description-services "
-            >{{ page_contents.description }}</p>
-            <service-list></service-list>
+            <v-lazy
+                v-if="ready"
+                :options="{threshold: 1}"
+                min-height="50px"
+                transition="scroll-x-transition"
+            >
+                <h1 class="text-sm-justify text-center">{{ title }}</h1>
+            </v-lazy>
+
+            <v-lazy
+                v-if="ready"
+                :options="{
+                threshold: 1
+            }"
+                min-height="150px"
+                transition="scroll-x-transition"
+            >
+                <p class="description-services ">{{ page_contents.description }}</p>
+            </v-lazy>
+            <v-lazy
+                v-if="ready"
+                :options="{
+                threshold: 1}"
+                min-height="300px"
+                transition="scroll-x-transition"
+            >
+                <service-list :services="services"></service-list>
+            </v-lazy>
         </v-container>
     </div>
 </template>
 
 <script>
 import ServiceList from "@/components/ServiceList.vue";
+import { mapState, mapActions, mapGetters } from "vuex";
 
 export default {
     name: "Service",
+    components: { ServiceList },
     props: {
         title: String,
         images: Object,
-        page_contents: Object
+        page_contents: Object,
     },
-    components: { ServiceList }
+    computed: {
+        ...mapState("services", {
+            services: "items"
+        }),
+        ...mapGetters("services", { readyServices: "ready" }),
+        ...mapGetters("pages", { readyPages: "ready" }),
+        ready() {
+            return this.readyPages ;
+        }
+    },
+    methods: {
+        ...mapActions("services", {
+            getServices: "fetchServices"
+        })
+    },
+    mounted() {
+        this.getServices();
+    }
 };
 </script>
 
